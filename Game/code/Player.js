ca08ba592c55b52623e6407cdf044828495da12f
Programer171 <86323466+Programer171@users.noreply.github.com>
2022-04-14 10:51:06 -0700

    Co-authored-by: James Floyd <j@floyd.com>

--- Game/code/Player.js
+++ Game/code/Player.js
@@ -1,48 +1,42 @@
 class Player{
 
      constructor(){
-
-        this.xSpeed = 0;
-        this.ySpeed = 0;
-        this.x = 0;
-        this.y = 0;
-        this.width = 50;
-        this.height = 100;
-        this.color = "red";
-        this.direction = 1;
-        this.jump = false;
-        this.jumpPower = 30;
-        this.fallingSpeed = 2;
-        this.minHeight = 90;
-        this.acceleration = 2;
-
+        this.xSpeed = 0;//this stores the horizontal speed of the player
+        this.ySpeed = 0;//this stores the vertical speed of the player
+        this.x = 0;//this is the x position of the player
+        this.y = 0;//this is the y position of the player
+        this.width = 50;//width of player
+        this.height = 100;//height of player
+        this.color = "red";//color of the player TODO: make this an image
+        this.direction = 1;//direction of the player this determines if the player is moving up or down
+        this.jump = false;//this isnt needed idk why i put it here lol
+        this.jumpPower = 30;//this is the power of the jump this determines how high the player can go wh
+        this.fallingSpeed = 2;//gravity
+        this.minHeight = 90;//this is the height of the ground
+        this.acceleration = 2;//this is the acceleration of the player
      }
-
+    //this function displays the player, it is run everyframe to show movement
      display(){
-
         fill(this.color);
         rect(this.x, this.y, this.width, this.height);
-
      }
-
+    ///this updates the players position and applies gravity
      update(){
-
+         if(this.x < 0 || this.x + 50 > width)
+            this.xSpeed = !this.xSpeed;
         this.x += this.xSpeed;
         this.y += this.ySpeed;
         this.gravity();
-     
     }
-
+    // this function applies gravity by changing the velocity of the player
      gravity(){
-
         if(this.y + 100>= height && this.jump == false){
             this.y = height - 130;
         }else{
             this.ySpeed = this.ySpeed + (this.acceleration * this.direction);
         }        
-
     }
-
+    //jump function
     Jump(){
         this.ySpeed = -this.jumpPower;       
     }
--- Game/code/sketch.js
+++ Game/code/sketch.js
@@ -1,23 +1,57 @@
-let player;
+let player;//player object
+let bg;// background image
+let grass;
+let imgAspect = 960/540;
+let bgWidth, bgHeight;
 
+function preload() {
+  // runs once to load all the files needed
+  bg = loadImage('../Assets/morning.png');//loadImage 
+  grass = loadImage('../Assets/grass.png');//loadImage
+}
+
+//this function runs once everytime the page is reloaded or opened like the main function in java
 function setup() {
   player = new Player();
   createCanvas(windowWidth, windowHeight);
 }
 
-
+//this function runs every frame(60 times per second) this will be the function we would be working on the most its kind of like a while(true) but better :D 
 function draw() {
-  background(220);
+  imageMode(CENTER);
+  if(imgAspect> width/height){
+    bgWidth=  height * imgAspect;
+    bgHeight =  height;
+  }
+  else{
+    bgWidth = width;
+    bgHeight = width/imgAspect;
+  }
+  image(bg,width/2,height/2, bgWidth,bgHeight);
+  imageMode(CORNER)
+  for(let i = 0; i<width; i+=100){
+    image(grass, i, height-100, 100,100);
+  }
   player.update();
   player.display();
 }
 
 
-
+//this function basically changes the size of the canvas(the drawing board) everytime the window is resized
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
+
+  if(imgAspect> width/height){
+    bgWidth=  height * imgAspect;
+    bgHeight =  height;
+  }
+  else{
+    bgWidth = width;
+    bgHeight = width/imgAspect;
+  }
 }
 
+//runs everytime a key is pressed
 function keyPressed(){
   if(keyCode === LEFT_ARROW){
    player.xSpeed = -5;
